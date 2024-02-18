170df7f8602087a263a126ebce7d979f2f606819
jcgoodwin86 <jcgoodwin86@users.noreply.github.com>
2024-02-18 15:29:22 -0500

    BB-30 Updated comments in SpendingList component

--- src/components/SpendingList/SpendingList.jsx
+++ src/components/SpendingList/SpendingList.jsx
@@ -1,13 +1,9 @@
-// SpendingList component is used to display the spending list for each account in the AccountList component.
-// It's used in App.js to display the spending list for each account.
+// SpendingList component is used to generate the spending list.
 // It takes one prop: spendingData.
 import SpendingBar from "../SpendingBar/SpendingBar";
 import styles from "./SpendingList.module.css";
 import { setUsdCurrency } from "../../utilities";
 
-// The component takes a spendingData prop. I put some default values in so I could test and build it.
-// Because I'm not sure how we will be rendering this prop. Once we start using it in the
-// app, we can remove the default values.
 export default function SpendingList({ spendingData = [{category: "Rent", spent: "1450"},{category: "Food", spent: "300"}, {category: "Netflix", spent: "20"}] }) {
     // max is the maximum amount spent in any category
     let max = Math.max(
