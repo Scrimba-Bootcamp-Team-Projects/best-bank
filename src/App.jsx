f2d1970df81a7f5e39c08245cfcd4b20eeba011f
jbarriguete86 <104594909+jbarriguete86@users.noreply.github.com>
2024-02-19 15:58:26 -0600

    BB-32 Design the login page and create a login component
    I refactor the App.jsx to first render the login page and then create a state with the userData that can be used to populate the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import React from "react"
 import Header from "./components/Header/Header";
+import Login from './pages/Login'
 import HomePage from "./pages/HomePage";
 import Payments from "./pages/Payments";
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 
 export default function App() {
+  const [userData, setUserData] = React.useState({})
 
-  return (
+  console.log(userData)
+
+  function handleSubmit(data){
+    setUserData(prevUser =>({
+      ...prevUser,
+      ...data
+    }))
+  }
+
+
+  return Object.keys(userData).length > 0 ? (
     <BrowserRouter>
       <Header />
       <Routes>
@@ -14,5 +27,7 @@ export default function App() {
           <Route path="/payments" element={<Payments />} />
       </Routes>
     </BrowserRouter>
-  );
+  ):
+  (<Login 
+  onSubmit={handleSubmit}/>)
 }
